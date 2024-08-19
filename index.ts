8f3e2e5473445da7e82c59faa0bcfe81ed2ae059
Vladimir Zhelvis <v.zhelvis@adguard.com>
2024-08-19 18:47:48 +0300

    fix merging and update data

--- index.ts
+++ index.ts
@@ -23,6 +23,35 @@ const requestInit: RequestInit = {
   },
 };
 
+
+function isObject(item: unknown): item is Record<string, unknown> {
+  return  item != null && item.constructor.name === 'Object';
+}
+
+
+export function mergeDeep(target: unknown, ...sources: unknown[]) {
+  if (!sources.length) {
+    return target
+  };
+
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) {
+          Object.assign(target, { [key]: {} })
+        };
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
+
 async function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 };
@@ -109,7 +138,7 @@ async function main() {
 
   for (const chunk of idsChunks) {
     const chunkTokensMetadataMap = await getTokensMetadataMap(chunk);
-    Object.assign(tokensMetadataMap, chunkTokensMetadataMap);
+    mergeDeep(tokensMetadataMap, chunkTokensMetadataMap);
     await sleep(2000);
   }
 
